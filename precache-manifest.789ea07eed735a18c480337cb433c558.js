c6f09f916e7619fc9098452c6fe60de5ec964902
FranHainesZone <52914701+FranHainesZone@users.noreply.github.com>
2021-04-29 07:57:50 +0100

    Updates

---
+++ precache-manifest.789ea07eed735a18c480337cb433c558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c4242a1befc9807db40",
+    "url": "/movie-mania/static/js/runtime~main.8b3ceade.js"
+  },
+  {
+    "revision": "afe546a6fa6c7d3196f4",
+    "url": "/movie-mania/static/js/main.49333a15.chunk.js"
+  },
+  {
+    "revision": "086781e99ca99a0c8b29",
+    "url": "/movie-mania/static/js/2.8065cee6.chunk.js"
+  },
+  {
+    "revision": "afe546a6fa6c7d3196f4",
+    "url": "/movie-mania/static/css/main.78665ff8.chunk.css"
+  },
+  {
+    "revision": "56c0e3f415f93103632d38e430afd867",
+    "url": "/movie-mania/index.html"
+  }
+];
